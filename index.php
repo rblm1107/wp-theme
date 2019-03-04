f1b752c91d9cbca2558d4ac438d7da5b13ca5a48
Rahma Belmustapha <rbelmustapha@gmail.com>
2019-03-04 16:11:07 +0100

    add templates>banner.php et placer le header dedans et importer grâce a get_templates_part

--- index.php
+++ index.php
@@ -1,17 +1,11 @@
 <?php
-get_header();
 // on a deplacé tte la partie head et la nav dans un fichier header.php et les remplacer par cette fonction dans l'index
-?>
+get_header();
+get_template_part('templates/banner');
 
+?>
   <!-- Header -->
-  <header class="masthead bg-primary text-white text-center">
-    <div class="container">
-      <img class="img-fluid mb-5 d-block mx-auto" src="wp-content/themes/rahma-theme/img/profile.png" alt="">
-      <h1 class="text-uppercase mb-0">Start Bootstrap</h1>
-      <hr class="star-light">
-      <h2 class="font-weight-light mb-0">Web Developer - Graphic Artist - User Experience Designer</h2>
-    </div>
-  </header>
+
 
   <!-- Portfolio Grid Section -->
   <section class="portfolio" id="portfolio">
