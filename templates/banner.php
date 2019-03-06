c63e484638a54729b4f0e33113725440bb1c78c3
Rahma Belmustapha <rbelmustapha@gmail.com>
2019-03-06 11:02:12 +0100

    remplacer le titre ainsi que la desription par la fonction get_bloginfo('nom')

--- templates/banner.php
+++ templates/banner.php
@@ -1,8 +1,14 @@
 <header class="masthead bg-primary text-white text-center">
     <div class="container">
       <img class="img-fluid mb-5 d-block mx-auto" src="wp-content/themes/rahma-theme/img/profile.png" alt="">
-      <h1 class="text-uppercase mb-0">Start Bootstrap</h1>
+      <h1 class="text-uppercase mb-0">
+        <!-- on remplace le titre par cette fonction afin de pouvoir le changer dans personalisation -->
+        <?php bloginfo('start Bootstrap');?>
+      </h1>
       <hr class="star-light">
-      <h2 class="font-weight-light mb-0">Web Developer - Graphic Artist - User Experience Designer</h2>
+      <h2 class="font-weight-light mb-0">
+          <!-- paraille pour la description -->
+         <?php bloginfo('Web Developer - Graphic Artist - User Experience Designer');?>
+      </h2>
     </div>
   </header>
